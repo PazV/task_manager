09ff80bf5d35d2d6257eb65dbd5e5dd1e5aec032
Paz <pgarcia@russellbedford.mx>
2018-07-16 10:54:22 -0500

    Archivo de configuración

---
+++ views/app_config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+postgres={
+    'dbname':'test',
+    'user':'postgres',
+    'passwd':'zedberto'
+}
+
+#task_path='/usr/local/arctic/tmp/'
+task_path='/tmp/task_manager/'
+host='http://127.0.0.1:9090'
+log_path='/var/log/project/'
