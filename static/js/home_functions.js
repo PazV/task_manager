66775b169e6aed1a245aee0560bba1b8709bec20
Paz Garcia <pgarcia@russellbedford.mx>
2018-08-20 17:02:16 -0500

    Agregar confirmación antes de cerrar sesión

--- static/js/home_functions.js
+++ static/js/home_functions.js
@@ -1,15 +1,29 @@
 $(document).ready(function(){
     $("#btn_log_off").on('click',function(){
-        console.log("click boton cerrar sesion");
-        $.ajax({
-            url:'/auth/logout',
-            data:'aaa',
-            type:'POST',
-            success:function(response){
-                console.log(response);
-            },
-            error:function(error){
-                console.log(error);
+        $.confirm({
+            theme:'dark',
+            title:'Atención',
+            content:'Está a punto de salir, ¿desea continuar?',
+            buttons:{
+                confirm:{
+                    text:'Sí',
+                    action:function(){
+                        $.ajax({
+                            url:'/auth/logout',
+                            data:'aaa',
+                            type:'GET',
+                            success:function(response){
+                                location.reload();
+                            },
+                            error:function(error){
+                                console.log(error);
+                            }
+                        });
+                    }
+                },
+                cancel:{
+                    text:'No'
+                }
             }
         });
     });
