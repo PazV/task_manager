fc98e5fa9446d8eaad32e8d7a0598c3cab21e6d7
Paz Garcia <pgarcia@russellbedford.mx>
2018-09-12 13:25:24 -0500

    Prueba server name, cookie path 1

--- __init__.py
+++ __init__.py
@@ -32,8 +32,8 @@ def create_app(test_config=None):
         SECRET_KEY=cfg.app_secret_key,
         #DATABASE=os.path.join(app.instance_path, 'taskapp.sqlite'),
         DEBUG_TB_INTERCEPT_REDIRECTS=False,
-        # SERVER_NAME=cfg.host,
-        # SESSION_COOKIE_PATH='/home'
+        SERVER_NAME=cfg.host,
+        SESSION_COOKIE_PATH='/home'
     )
     mail = Mail(app)
     # toolbar=DebugToolbarExtension(app)
